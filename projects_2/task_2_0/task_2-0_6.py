5da064ba6921cc7b0c284674f8ed428020d1881a
Sonya <abramovasg2015@gmail.com>
2026-02-11 22:17:01 +0300

    Add files via upload

---
+++ projects_2/task_2_0/task_2-0_6.py
@@ -0,0 +1,2 @@
+with open("C:/Users/Abramova/output.txt", "w", encoding="utf-8") as f:
+    print("Меня зовут Абрамова София\nЯ учусь в группе 4731204_50001 биомедицинских систем и биотехнологий\nМне 18 лет", file=f)
